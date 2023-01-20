12f4cb8a742820313e0a580357ec2b9666ea591e
sagm3012 <asadbeksobirov2002@gmail.com>
2023-01-20 23:29:35 +0500

    Добавьте файлы проекта.

---
+++ amp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleDFT
+{
+    internal class amp
+    {
+        internal static void Add(object ampp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ phase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleDFT
+{
+    internal class phase
+    {
+        internal static void Add(object phasse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
